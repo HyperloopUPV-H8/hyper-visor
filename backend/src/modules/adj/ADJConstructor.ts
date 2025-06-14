2e4da2f873fbf5db98fc0d09b896b5224f2bf82a
Andres de la Torre Mora <andelatmo@gmail.com>
2025-06-14 20:32:16 +0200

    feat: ADJConstructor.ts

--- backend/src/modules/adj/ADJConstructor.ts
+++ backend/src/modules/adj/ADJConstructor.ts
@@ -3,7 +3,7 @@ import { promises as fs } from 'fs';
 import { err, ok, Result } from 'neverthrow';
 import * as path from 'path';
 import { ADJLectureError } from './errors/ADJLecture';
-import { Board, BoardName } from 'types/adj';
+import { Board, BoardName, Measurement, Packet } from 'types/adj';
 
 export class ADJConstructor {
     private _adj!: ADJ;
@@ -16,6 +16,7 @@ export class ADJConstructor {
 
     public async execute(): Promise<ADJ> {
         const boardsDefinitionFilePath = path.join(this._adjPath, 'boards.json');
+
         const registerBoardsResult = await this.registerBoards(boardsDefinitionFilePath);
 
         if (registerBoardsResult.isErr()) {
@@ -23,6 +24,16 @@ export class ADJConstructor {
         }
 
         const registerPacketsResult = await this.registerPackets();
+
+        if (registerPacketsResult.isErr()) {
+            throw registerPacketsResult.error;
+        }
+
+        const registerMeasurementsResult = await this.registerMeasurements();
+
+        if (registerMeasurementsResult.isErr()) {
+            throw registerMeasurementsResult.error;
+        }
 
         return this._adj;
     }
@@ -50,19 +61,50 @@ export class ADJConstructor {
     
     private async registerPackets(): Promise<Result<void, ADJLectureError>> {
         for (const board of this._adj.boards) {
-            const packetsFilePath = path.join(this._adjPath, 'boards', board.name, 'packets.json');
-            console.log(packetsFilePath);
-            try {
-                const packetsFile = await fs.readFile(packetsFilePath);
-                const packetsObj = JSON.parse(packetsFile.toString());
-                const packets = Object.keys(packetsObj);
-                return ok();
-            } catch (error) {
-                return err(new ADJLectureError(`Error while reading packets file: ${error}`, error as Error, packetsFilePath));
+            const boardDir = await fs.readdir(path.join(this._adjPath, 'boards', board.name));
+            const packetsFiles = boardDir.filter((file: string) => /packets.*\.json$/.test(file));
+            const packetsFilesPaths = packetsFiles.map((file: string) => path.join(this._adjPath, 'boards', board.name, file));
+
+            for (const packetsFilePath of packetsFilesPaths) {
+                try {
+                    const packets = await this.buildPacketsFromFile(packetsFilePath);
+                    if (packets.isErr()) {
+                        return err(packets.error);
+                    } else {
+                        for (const packet of packets.value) {
+                            this._adj.addPacket(packet);
+                        }
+                    }
+                } catch (error) {
+                    return err(new ADJLectureError(`Error while reading packets file: ${error}`, error as Error, packetsFilePath));
+                }
             }
         }
+        return ok();
+    }
 
-        return err(new ADJLectureError('No boards found to register packets', new Error('No boards found to register packets'), ''));
+    private async registerMeasurements(): Promise<Result<void, ADJLectureError>> {
+        for (const board of this._adj.boards) {
+            const boardDir = await fs.readdir(path.join(this._adjPath, 'boards', board.name))
+            const measurementsFiles = boardDir.filter((file: string) => /measurements.*\.json$/.test(file))
+            const measurementsFilesPaths = measurementsFiles.map((file: string) => path.join(this._adjPath, 'boards', board.name, file))
+
+            for (const measurementsFilePath of measurementsFilesPaths) {
+                try {
+                    const measurements = await this.buildMeasurementsFromFile(measurementsFilePath);
+                    if (measurements.isErr()) {
+                        return err(measurements.error);
+                    } else {
+                        for (const measurement of measurements.value) {
+                            this._adj.addMeasurement(measurement);
+                        }
+                    }
+                } catch (error) {
+                    return err(new ADJLectureError(`Error while reading measurements file: ${error}`, error as Error, measurementsFilePath));
+                }
+            }
+        }
+        return ok();
     }
 
     private async buildBoardFromFile(boardName: BoardName, path: string): Promise<Result<Board, ADJLectureError>> {
@@ -82,6 +124,47 @@ export class ADJConstructor {
         } catch (error) {
             return err(new ADJLectureError(`Error while reading board info file: ${error}`, error as Error, path));
         }
+    }
 
+    private async buildPacketsFromFile(path: string): Promise<Result<Packet[], ADJLectureError>> {
+        const packets: Packet[] = [];
+        try {
+            const packetsFile = await fs.readFile(path);
+            const packetsObj = JSON.parse(packetsFile.toString());
+            for (const packet of packetsObj) {
+                const newPacket: Packet = {
+                    id: packet.id,
+                    type: packet.type,
+                    name: packet.name,
+                    variables: packet.variables,
+                }
+                packets.push(newPacket);
+            }
+            return ok(packets);
+        } catch (error) {
+            return err(new ADJLectureError(`Error while reading packets file: ${error}`, error as Error, path));
+        }
+    }
+
+
+    private async buildMeasurementsFromFile(measurementsFilePath: string): Promise<Result<Measurement[], ADJLectureError>> {
+        const measurements: Measurement[] = [];
+        try {
+            const measurementsFile = await fs.readFile(measurementsFilePath);
+            const measurementsObj = JSON.parse(measurementsFile.toString());
+            for (const measurement of measurementsObj) {
+                const newMeasurement: Measurement = {
+                    id: measurement.id,
+                    name: measurement.name,
+                    type: measurement.type,
+                    podUnits: measurement.podUnits,
+                    displayUnits: measurement.displayUnits,
+                }
+                measurements.push(newMeasurement);
+            }
+            return ok(measurements);
+        } catch (error) {
+            return err(new ADJLectureError(`Error while reading measurements file: ${error}`, error as Error, measurementsFilePath));
+        }
     }
 }
