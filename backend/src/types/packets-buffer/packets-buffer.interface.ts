06b510631096df8c0ebd5a68fecd8d60f98d00ea
Andres de la Torre Mora <andelatmo@gmail.com>
2025-06-15 16:09:45 +0200

    feat: packets buffer interface

--- backend/src/types/packets-buffer/packets-buffer.interface.ts
+++ backend/src/types/packets-buffer/packets-buffer.interface.ts
@@ -1,4 +1,24 @@
 export interface IPacketsBuffer {
+    /**
+     * The internal buffer to store the packets to be sended to the subscribers
+     */
     buffer: Buffer;
-    
+
+    /**
+     * Write a packet to the buffer
+     * @param packet - The packet to be written to the buffer
+     */
+    write(packet: any): void;
+
+    /**
+     * Subscribe to the buffer to receive packets
+     * @param callback - The callback to be called when a packet is written to the buffer
+     */
+    subscribe(callback: (packet: any) => void): void;
+
+    /**
+     * Unsubscribe from the buffer
+     * @param callback - The callback to be removed from the buffer
+     */
+    unsubscribe(callback?: (packet: any) => void): void;
 }
