3e90d2be14065ee86e8ed409dc13f03d8681bed8
Andres de la Torre Mora <andelatmo@gmail.com>
2025-08-20 23:53:17 +0200

    add logo to header

--- hyper-visor-app/src/layout/Header/Header.tsx
+++ hyper-visor-app/src/layout/Header/Header.tsx
@@ -1,8 +1,12 @@
 import styles from './Header.module.scss'
+import logo from '../../assets/hyperloop_logo.svg'
 
 export const Header = () => {
     return (
         <div className={styles.container}>
+            <div className={styles.logoContainer}>
+                <img src={logo} alt="Hyperloop UPV" className={styles.logo} />
+            </div>
             <h1 className={styles.title}>Hyperloop UPV</h1>
         </div>
     )
